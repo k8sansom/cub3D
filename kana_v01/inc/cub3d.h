25e1d1c08b12f82261bb3ba35326928ac2fb67ec
Kathryn Sansom <ksansom@c3a5c1.42berlin.de>
2024-05-07 10:23:21 +0200

    adding kana_v01

---
+++ kana_v01/inc/cub3d.h
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksansom <ksansom@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 10:17:30 by ksansom           #+#    #+#             */
+/*   Updated: 2024/05/07 10:17:31 by ksansom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../mlx/mlx.h"
+# include "../libft/inc/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include "../mlx/mlx.h"
+# include "../mlx/mlx_int.h"
+# include <math.h>
+# include <fcntl.h>
+
+# define IMG_W	32
+# define IMG_H	32
+
+# define WALL_XPM			"assets/wall.xpm"
+# define FLOOR_XPM			"assets/floor.xpm"
+# define WINE_XPM			"assets/wine.xpm"
+# define EXIT_XPM			"assets/toilet.xpm"
+# define PLAYER_DOWN_XPM	"assets/player_down.xpm"
+# define PLAYER_UP_XPM		"assets/player_up.xpm"
+# define PLAYER_RIGHT_XPM	"assets/player_right.xpm"
+# define PLAYER_LEFT_XPM	"assets/player_left.xpm"
+# define ENEMY_XPM			"assets/enemy.xpm"
+
+# define KEY_W		119
+# define KEY_A		97
+# define KEY_S		115
+# define KEY_D		100
+# define KEY_ESC	65307
+# define KEY_UP  	65362
+# define KEY_LEFT  	65361
+# define KEY_RIGHT 	65363
+# define KEY_DOWN  	65364	
+
+typedef struct s_image
+{
+	void	*xpm_ptr;
+	int		x;
+	int		y;
+}	t_image;
+
+typedef struct s_game
+{
+	int		fd;
+	int		map_height;
+	int		map_width;
+	int		player_counter;
+	int		collectable_counter;
+	int		move_counter;
+	int		exit_counter;
+	int		x_axis;
+	int		y_axis;
+	int		exit_code;
+
+	char	**map;
+
+	t_image	floor;
+	t_image	wall;
+	t_image	player_down;
+	t_image	player_up;
+	t_image	player_right;
+	t_image	player_left;
+	t_image	exit;
+	t_image	wine;
+	t_image	enemy;
+
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_game;
+
+#endif

142e941c975e09d6f48d84ea9af3745743aa54b7
Anastasiia Voronkova <138679587+delichon-urbica@users.noreply.github.com>
2024-05-10 14:36:41 +0200

    Add files via upload

--- kana_v01/inc/cub3d.h
+++ kana_v01/inc/cub3d.h
@@ -6,20 +6,23 @@
 /*   By: avoronko <avoronko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:17:30 by ksansom           #+#    #+#             */
-/*   Updated: 2024/05/09 15:07:52 by avoronko         ###   ########.fr       */
+/*   Updated: 2024/05/10 12:38:54 by avoronko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../minilibx-linux/mlx.h"
+//# include "../mlx/mlx.h"
 # include "../libft/inc/libft.h"
 # include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
-# include "../minilibx-linux/mlx_int.h"
+//# include "../mlx/mlx.h"
+//# include "../mlx/mlx_int.h"
 # include <math.h>
 # include <fcntl.h>
+# include <sys/errno.h>
 
 # define IMG_W	32
 # define IMG_H	32
@@ -42,57 +45,89 @@
 # define KEY_UP  	65362
 # define KEY_LEFT  	65361
 # define KEY_RIGHT 	65363
-# define KEY_DOWN  	65364
-# define FOV		(60 * (PI / 180))
-# define PI			3.14159265358979323846
+# define KEY_DOWN  	65364	
 
-typedef struct s_image
+typedef struct s_ray
 {
-	void	*xpm_ptr;
-	int		x;
-	int		y;
-}	t_image;
-
-typedef struct s_game
-{
-	int		fd;
-	int		map_height;
-	int		map_width;
-	int		player_counter;
-	int		exit_counter;
-	int		player_x;
-	int		player_y;
-	int		exit_code;
-
-	char	**map;
-
-	t_image	floor;
-	t_image	wall;
-	t_image	player;
-	t_image	exit;
-	t_image	wine;
-
-	void	*mlx_ptr;
-	void	*win_ptr;
-
 	bool	hit_wall;
 	int		current_column;
 	double	wall_dist;
-	double	ray_pos_x;
-	double	ray_pos_y;
-	double	ray_dir_x;
-	double	ray_dir_y;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
 	double	delta_dist_x;
 	double	delta_dist_y;
 	double	side_dist_x;
 	double	side_dist_y;
 	int		step_x;
 	int		step_y;
+}	t_ray;
 
+typedef struct s_textures
+{
+	char			*north;
+	char			*south;
+	char			*west;
+	char			*east;
+	int				*floor;
+	int				*ceiling;
+	size_t			hex_floor;
+	size_t			hex_ceiling;
+	int				size;
+	int				index;
+	double			step;
+	double			pos;
+	int				x;
+	int				y;
+}	t_textures;
+
+typedef struct s_player
+{
+	char	dir;
+	double	pos_x;
+	double	pos_y;
 	double	dir_x;
 	double	dir_y;
 	double	plane_x;
 	double	plane_y;
+	int		has_moved;
+	int		move_x;
+	int		move_y;
+	int		rotate;
+}	t_player;
+
+typedef struct s_game
+{
+	int			fd;
+	// void		*mlx_ptr;
+	// void		*win_ptr;
+	int			win_height;
+	int			win_width;
+	int			map_height;
+	int			cub_height;
+	int			map_width;
+
+	char		**cub_file;
+	char		**map;
+
+	t_textures	textures;
+	t_player	player;
+	t_ray		ray;
 }	t_game;
+
+//init
+void	init_game(t_game *game);
+
+//parsing
+void	parse_file(t_game *game, char *map);
+void 	read_cub(t_game *game, char *map);
+
+//errors
+void	full_exit(char *s, t_game *game, int exit_code);
+void	free_arr(char **arr);
+
+//textures
+void	read_textures(t_game *game);
 
 #endif

54a119c24e0182c40df26d5ec06accc43a542e65
Kathryn Sansom <ksansom@c3a8c6.42berlin.de>
2024-05-21 11:00:33 +0200

    combined

--- kana_v02/src/main.c
+++ kana_v02/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ksansom <ksansom@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:19:32 by ksansom           #+#    #+#             */
-/*   Updated: 2024/05/21 10:41:01 by ksansom          ###   ########.fr       */
+/*   Updated: 2024/05/21 10:52:31 by ksansom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,12 @@ int	main(int ac, char **av)
 	init_game(&game);
 	parse_file(&game, av[1]);
 	init_mlx(&game);
-	// init_texture_arr(&game);
-	// init_player_dir(&game);
-	// init_minimap(&game);
-	// set_hooks(&game);
-	// mlx_loop_hook(game.mlx_ptr, render_game, &game);
-	// mlx_loop(game.mlx_ptr);
+	init_texture_arr(&game);
+	init_player_dir(&game);
+	init_minimap(&game);
+	set_hooks(&game);
+	mlx_loop_hook(game.mlx_ptr, render_game, &game);
+	mlx_loop(game.mlx_ptr);
 	exit(full_exit("all done", &game, 0));
 	return (0);
 }

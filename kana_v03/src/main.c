14f8e5e2bcfbd448f6ef3877a8e346dc19919108
Anastasiia Voronkova <138679587+delichon-urbica@users.noreply.github.com>
2024-05-24 14:20:30 +0200

    corrected while loops in rendering/raycasting
    but guess who is still not able to draw walls.

--- kana_v03/src/main.c
+++ kana_v03/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ksansom <ksansom@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:19:32 by ksansom           #+#    #+#             */
-/*   Updated: 2024/05/24 12:39:40 by ksansom          ###   ########.fr       */
+/*   Updated: 2024/05/24 12:19:44 by ksansom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	main(int ac, char **av)
 	}
 	init_game(&game);
 	parse_file(&game, av[1]);
+	printf("%s\n", game.textures.north);
+	printf("what about here?\n");
 	init_mlx(&game);
 	init_texture_arr(&game);
 	init_player_dir(&game);
